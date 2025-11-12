ce94265662477aa3eab11a5cebd55624603cc4de
NutanSarnobat23 <nutansarnobat2305@gmail.com>
2025-11-12 17:17:52 +0000

    dataTypes

---
+++ .devcontainer/01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict";//treat all JS code as newer version
+
+//alert(3+3)//we are using node.js not browser
+
+console.log(3
+    +
+    3)//code readaibility should be very high
+
+
+console.log="hitesh"
+
+let name='hitesh'
+let age=18
+let isLoggedIn=false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string=""
+// boolean =>true/false
+// null =>standalone value
+// undefined
+// symol =>unique
+
+//object
+
+console.log(typeof undefined);//undefined
+console.log(typeof null);//object
